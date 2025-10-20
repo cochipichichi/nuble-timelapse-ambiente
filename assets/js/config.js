49f79211cb14ec563899ef954adb29772dcdcdac
Ragnar <cochipichichi@gmail.com>
2025-10-19 23:08:15 -0300

    Add files via upload

---
+++ assets/js/config.js
@@ -0,0 +1,8 @@
+
+// Edit these endpoints if you have official WMTS keys/sources.
+window.NUBLE_LAYERS = {
+  // EOX Sentinel-2 Cloudless (approximate public XYZ pattern)
+  s2cloudless: 'https://tiles.maps.eox.at/wmts/1.0.0/s2cloudless-2020_3857/GoogleMapsCompatible/{z}/{x}/{y}.jpg',
+  // FAO/ESA WorldCover placeholder (replace with valid XYZ/WMTS from your provider)
+  worldcover:  'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+};
