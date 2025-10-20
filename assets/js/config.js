c952447c70a2b67bdb61501938e23f4b08f518cf
Ragnar <cochipichichi@gmail.com>
2025-10-19 23:28:39 -0300

    Add files via upload

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,5 +1,6 @@
-
 window.NUBLE_LAYERS = {
-  s2cloudless: 'https://tiles.maps.eox.at/wmts/1.0.0/s2cloudless-2020_3857/GoogleMapsCompatible/{z}/{x}/{y}.jpg',
-  worldcover:  'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+  s2cloudless_2018: 'https://tiles.maps.eox.at/wmts/1.0.0/s2cloudless-2018_3857/GoogleMapsCompatible/{z}/{x}/{y}.jpg',
+  s2cloudless_2020: 'https://tiles.maps.eox.at/wmts/1.0.0/s2cloudless-2020_3857/GoogleMapsCompatible/{z}/{x}/{y}.jpg',
+  worldcover:  'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+  erosion_wmts: 'REEMPLAZAR_AQUI_WMTS_RIESGO'  // placeholder
 };
